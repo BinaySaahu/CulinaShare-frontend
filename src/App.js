005821e9510a769868c26b7e706b4691a93588b3
BinaySaahu <100080926+BinaySaahu@users.noreply.github.com>
2024-02-23 01:47:33 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import NavBar from './components/NavBar';
@@ -10,9 +10,12 @@ import MyRecipePage from './pages/MyRecipePage';
 import RecipeDescPage from './pages/RecipeDescPage';
 import axios from 'axios';
 import AboutUs from './pages/AboutUs';
+import { useSelector } from 'react-redux';
+import ProtectedRoutes from './ProtectedRoutes';
 
 function App() {
   // axios.defaults.baseURL = "http://localhost:8001/"
+  const user = useSelector(state => state.user)
   return (
     <div className="">
       <NavBar/>
@@ -20,11 +23,13 @@ function App() {
       <Menu/>
       <Routes>
         <Route path='/' element = {<AuthPage/>}/>
-        <Route path='/home' element = {<HomePage/>}/>
-        <Route path='/favorites' element = {<FavoritesPage/>}/>
-        <Route path='/myRecipes' element = {<MyRecipePage/>}/>
-        <Route path='/desc/:id' element = {<RecipeDescPage/>}/>
-        <Route path='/about' element = {<AboutUs/>}/>
+        <Route element = {<ProtectedRoutes/>}>
+          <Route path='/home' element = {<HomePage/>} />
+          <Route path='/favorites' element = {<FavoritesPage/>}/>
+          <Route path='/myRecipes' element = {<MyRecipePage/>}/>
+          <Route path='/desc/:id' element = {<RecipeDescPage/>}/>
+          <Route path='/about' element = {<AboutUs/>}/>
+        </Route>
       </Routes>
     </div>
   );
